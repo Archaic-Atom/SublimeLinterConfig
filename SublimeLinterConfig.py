7b053404c7add737c3766c21589456457b80aa94
JackRao <raoxi36@foxmail.com>
2021-06-02 16:58:31 +0800

    v0.0.1 test

--- SublimeLinterConfig.py
+++ SublimeLinterConfig.py
@@ -5,19 +5,19 @@ import sublime_plugin
 
 class sublime_linter_config_open_file_actions(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        self.view.insert(edit, 0, "Hello, World1!")
 
 
 class sublime_linter_config_create_file_actions(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        self.view.insert(edit, 0, "Hello, World2!")
 
 
 class sublime_linter_config_switch_file_actions(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        self.view.insert(edit, 0, "Hello, World3!")
 
 
 class sublime_linter_config_remove_file_actions(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        self.view.insert(edit, 0, "Hello, World4!")
