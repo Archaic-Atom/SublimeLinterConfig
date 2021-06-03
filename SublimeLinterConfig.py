dcc6120d378d2eeeac834f814809af10faff1f3c
JackRao <raoxi36@foxmail.com>
2021-06-03 11:24:08 +0800

    v0.0.2 test

--- SublimeLinterConfig.py
+++ SublimeLinterConfig.py
@@ -29,6 +29,8 @@ class sublime_linter_config_open_file_actions(sublime_plugin.TextCommand):
     def _on_done(idx):
         # type: (int) -> None
         if idx < sys_def.INVAILD_ID:
+            sublime.message_dialog(
+                "SublimelinterComfig can't find the config files, please create it!")
             return
 
         sublime.message_dialog(sublime_linter_config_open_file_actions._RES_LIST[idx])
@@ -39,14 +41,14 @@ class sublime_linter_config_create_file_actions(sublime_plugin.TextCommand):
     _RES_LIST = []
 
     def run(self, edit):
+        # self.view.insert(edit, 0, "Hello, World2!")
         view = self.view
         window = view.window()
 
-        window.show_input_panel(sys_def.OUR_CAPTION, "",
+        window.show_input_panel('The sublimelinter config file name:', '',
                                 sublime_linter_config_create_file_actions._on_done,
                                 None,
                                 None)
-        # self.view.insert(edit, 0, "Hello, World2!")
 
     @staticmethod
     def _on_done(line_str: str):
@@ -54,7 +56,25 @@ class sublime_linter_config_create_file_actions(sublime_plugin.TextCommand):
         cfg_pth = os.path.join(pkg_path, sys_def.USER_FOLDER)
         sublime_linter_config_create_file_actions._RES_LIST = FlieHandler.find_file(
             cfg_pth, sys_def.FILE_NAME_FORMAT)
-        sublime.message_dialog(line_str)
+
+        line_str = line_str + sys_def.FILE_NAME_TYPE
+
+        if line_str in sublime_linter_config_create_file_actions._RES_LIST:
+            sublime.message_dialog("This config file already exists!")
+            return
+
+        sublime.run_command("new_window")
+        default_cfg_path = os.path.join(pkg_path, sys_def.DEFAULT_CONFIG_PATH)
+
+        window = sublime.active_window()
+        window.open_file(default_cfg_path)
+        view = window.new_file()
+        view.set_name(line_str)
+        window.run_command('set_layout',
+                           {"cols": [0.0, 0.5, 1.0],
+                            "rows": [0.0, 1.0],
+                            "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]}
+                           )
 
 
 class sublime_linter_config_switch_file_actions(sublime_plugin.TextCommand):
--- lib/sys_define.py
+++ lib/sys_define.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+# define name
+FILE_NAME_TYPE = '_sublimelinter_cfg.sublime-settings'
+INPUT_CAPTION = 'The sublimelinter config file name:'
+DEFAULT_INPUT_STR = ''
+DEFAULT_CONFIG_PATH = 'SublimeLinter/SublimeLinter.sublime-settings'
 
 # path
 USER_FOLDER = 'User/'
-FILE_NAME_FORMAT = '*.sublime-settings'
-OUR_CAPTION = 'The sublimelinter config name:'
+FILE_NAME_FORMAT = '*_sublimelinter_cfg.sublime-settings'
+
 
 # Encoder
 INVAILD_ID = 0
